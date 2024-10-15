6f24fd72b229c53b08fa6f25efc127ee69b730fa
Loviiin <ghvillarea12@gmail.com>
2024-10-15 16:22:48 -0300

    mlk q porra é essa

--- condoflow/core/views.py
+++ condoflow/core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+
+def avisos(request):
+    return render(request, "core/avisos.html")
+
+def registros(request):
+    return render(request, "core/registros.html")
+
+def cobranca(request):
+    return render(request, "core/cobranca.html")
+
+def reuniao(request):
+    return render(request, "core/reuniao.html")
+
+def reserva(request):
+    return render(request, "core/reserva.html")
+
+def configuracoes(request):
+    return render(request, "core/configuracoes.html")
+
+def login(request):
+    return render(request, "core/login.html")
