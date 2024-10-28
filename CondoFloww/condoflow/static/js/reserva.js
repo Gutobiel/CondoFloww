cfa63581fad39f5aa2eb38c965008f4ef4bf4992
Augusto Gabriel Rodrigues dos Santos <118448949+Gutobiel@users.noreply.github.com>
2024-10-28 19:55:42 -0300

    Delete CondoFloww/condoflow directory

--- CondoFloww/condoflow/static/js/reserva.js
+++
@@ -1,9 +0,0 @@
-function fazerReserva() {
-    const area = document.getElementById("area").value;
-    const data = document.getElementById("data").value;
-    const hora = document.getElementById("hora").value;
-  
-    // Aqui você pode realizar a lógica para fazer a reserva, por exemplo, exibindo um alerta
-    alert(`Reserva feita!\nÁrea: ${area}\nData: ${data}\nHorário: ${hora}`);
-  }
-  
